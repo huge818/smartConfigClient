d2cf216ab2e27aec3db7d0b4efbe5d6aa2ff2e2a
Einsy <huzc818@qq.com>
2017-10-01 23:37:40 +0800

    update java wif

--- src/android/smartConfigClient.java
+++ src/android/smartConfigClient.java
@@ -7,26 +7,124 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+
+import android.util.Log;
+import com.example.client.util.NetWorkUtil;
+import com.example.smartconfigclientdemo.R;
+import com.iflytek.smartconfig.client.SmartConfigClient;
+import com.iflytek.smartconfig.listener.RecvListener;
+import com.iflytek.smartconfig.message.NotifyMessage;
+
+
 /**
  * This class echoes a string called from JavaScript.
  */
 public class smartConfigClient extends CordovaPlugin {
+	private SendThread mSendThread;
+	private String mSSID;
+	private String mPassword;
+	private int mLocalIP;
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
-        }
-        return false;
-    }
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		if (action.equals("initConfig")) {
+			int packetInterval = args.getInt(0);
+			int pecvTimeOut = args.getInt(1);
+			SmartConfigClient.setPacketInterval(packetInterval);
+			SmartConfigClient.setRecvTimeOut(pecvTimeOut);
+			return true;
+		}
+		else if (action.equals("stopListen")) {
+			SmartConfigClient.stopListen();
+			return true;
+		}
+		else if(action.equals("startSendAndListen")) {
+			String ssid = args.getString(0);
+			String password = args.getString(1);
+			int localIP = args.getInt(2);
+			mSendThread = new SendThread(ssid, password, localIP);
+			mSendThread.start();
+			this.startListen(callbackContext);
+			return true;
+		}
+		else if(action.equals("startListen")){
+			this.startListen(callbackContext);
+			return true;
+		}
+		return false;
+	}
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
-    }
+	public void startListen(CallbackContext callbackContext){
+		RecvListener recvListener = new RecvListener() {
+			@Override
+			public void onReceiveTimeOut() {
+				JSONObject receiveTimeOut = new JSONObject();
+				ReceiveTimeOut.put("type", "receiveTimeOut");
+				sendPluginResult(callbackContext,receiveTimeOut);
+			}
+
+			@Override
+			public void onError(int errorCode) {
+				JSONObject error = new JSONObject();
+				error.put("type", "error");
+				error.put("errorCode", errorCode);
+				sendPluginResult(callbackContext,error);
+			}
+
+			@Override
+			public void onReceived(NotifyMessage message) {
+				JSONObject received = new JSONObject();
+				received.put("type", "received");
+				received.put("mac", message.getMac());
+				received.put("ip", message.getIp());
+				received.put("port", message.getPort());
+				received.put("HostName", message.getHostName());
+				sendPluginResult(callbackContext,received);
+			}
+		};
+		SmartConfigClient.startListen(recvListener);
+	}
+
+	public void sendPluginResult(CallbackContext callbackContext,JSONObject obj){
+		PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, obj);
+		pluginResult.setKeepCallback(true);
+		callbackContext.sendPluginResult(pluginResult);
+	}
+
+	class SendThread extends Thread {
+		public SendThread(String ssid, String password, int localIP) {
+			mSSID = ssid;
+			mPassword = password;
+			mLocalIP=localIP;
+		}
+		public SendThread(){}
+		
+		private boolean mStopRun = false;
+
+		public void stopRun() {
+			mStopRun = true;
+			interrupt();
+		}
+
+		@Override
+		public void run() {
+			while (!mStopRun) {
+				try{
+					SmartConfigClient.send(mSSID, mPassword, mLocalIP);
+				} catch (Exception e) {
+					Log.e(TAG, "Exception when send smartconfig info");
+				}
+			}
+		}
+	}
 }
+
+
